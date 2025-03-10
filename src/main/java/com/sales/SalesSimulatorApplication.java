6cfaa5ea0a47dcdc738a0b032da77c4b3d7e4eaa
klinton.daltio@gmail.com <@daltiosk77>
2025-03-10 06:27:02 -0300

    Initial Commit

---
+++ src/main/java/com/sales/SalesSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSimulatorApplication.class, args);
+	}
+
+}
